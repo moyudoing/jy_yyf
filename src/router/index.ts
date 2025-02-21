50826e275c0c34ff3a058597e35c49a3e053a6ac
moyu_master <2641346927@qq.com>
2025-02-21 19:06:28 +0800

    time: 2025/2/19 version: 0.00.03 router-update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
     {
@@ -56,7 +56,7 @@ const routes = [
 ];
 
 const mainRouter = createRouter({
-    history: createWebHistory('/jy_yyf/'),
+    history: createWebHashHistory('/jy_yyf/'),
     routes
 });
 
